0c4bebdbec0c83cc854baaba2a9e49c5014e8c06
kanotan <kanon.ayuayu@gmail.com>
2022-01-30 01:27:07 +0900

    devise_token_authの初期設定を実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 end
