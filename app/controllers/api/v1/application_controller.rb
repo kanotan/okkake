ad21466b7767c541bb05f86a07dcc7b7ed3dc92f
kanotan <kanon.ayuayu@gmail.com>
2022-01-30 22:20:48 +0900

    Rubocop対応

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
-    class ApplicationController < ActionController::API # Note: here is not ::BASE
+    # NOTE: here is not ::BASE
+    class ApplicationController < ActionController::API
       include DeviseTokenAuth::Concerns::SetUserByToken
       protect_from_forgery with: :null_session
     end
